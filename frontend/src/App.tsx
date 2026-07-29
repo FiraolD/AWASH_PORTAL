5b6c1950db5cb9208179b313852ea33050c3be83
Firaol Delesa <firaoldelesa18@gmail.com>
2026-07-29 09:20:03 +0300

    updated App.tsx file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,6 +13,7 @@ import FinalApprovalQueue from './pages/Underwriting/FinalApprovalQueue';
 
 
 
+
 // ============================================
 // AUTH PAGES
 // ============================================
